66a58e2b91fc822dc576cc50984438a4fa1fcef7
wai-calvin <wai.calvin96@gmail.com>
2020-11-19 15:47:36 -0800

    downgraded node-sass version and added simple components structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
+//Styles
+import "./styles/app.scss";
+//Components
+import Nav from "./components/Nav";
+import Body from "./components/Body";
+import Player from "./components/Player";
 
 function App() {
   return (
     <div className="App">
-      <h1>Iraya</h1>
+      <div className="navBar">
+        <Nav />
+      </div>
+      <Body />
+      <Player />
     </div>
   );
 }
